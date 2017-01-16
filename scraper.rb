a4eca8c4d432b44996e37a5f7249eec9ac397a8a
Tony Bowden <tony@tmtm.com>
2017-01-16 11:31:11 +0000

    Rewrite using Scraped

--- scraper.rb
+++ scraper.rb
@@ -14,40 +14,86 @@ def noko_for(url)
   Nokogiri::HTML(open(url).read)
 end
 
-def unbracket(str)
-  cap = str.match(/^(.*?)\s*\((.*?)\)\s*$/) or return [str, 'Independent']
-  cap.captures
+class MembersPage < Scraped::HTML
+  decorator Scraped::Response::Decorator::AbsoluteUrls
+
+  field :members do
+    noko.css('div.content table tr').map do |row|
+      fragment row => MemberRow
+    end
+  end
 end
 
-def scrape_list(url)
-  noko = noko_for(url)
-  count = 0
-  noko.css('div.content table tr').each do |row|
-    tds = row.css('td')
-    mp_url = URI.join(url, tds[1].at_css('a/@href').text).to_s
-    mp_noko = noko_for(mp_url)
-    data = {
-      id:        mp_url.split('/').last.sub('.aspx', ''),
-      name:      tds[1].css('a').text.tidy,
-      area:      tds[0].text.tidy,
-      party:     unbracket(tds[1].text.gsub(/[[:space:]]/, ' ').tidy).last,
-      executive: tds[2].text.tidy,
-      email:     mp_noko.at_css('div.content a[href*=mailto]/@href').to_s.gsub(/[[:space:]]/, ' ').tidy.sub('mailto:', ''),
-      term:      2012,
-      image:     mp_noko.css('li.PBItem div.content h1').xpath('./following::img[1]/@src').text,
-      source:    mp_url,
-    }
-    data[:party_id] = data[:party]
-    data[:area].sub!(/ C\*?$/, ' Central')
-    data[:area_id], data[:area] = data[:area].split(' - ', 2) if data[:area][/ - /]
-    data[:executive] = '' if data[:executive] == 'Backbencher'
-    # sigh http://stackoverflow.com/questions/13013987/ruby-how-to-escape-url-with-square-brackets-and
-    data[:image] = URI.join(mp_url, URI.encode(URI.decode(data[:image])).gsub('[', '%5B').gsub(']', '%5D')).to_s unless data[:image].to_s.empty?
-    ScraperWiki.save_sqlite(%i(id term), data)
-    count += 1
+class MemberRow < Scraped::HTML
+  field :id do
+    source.split('/').last.sub('.aspx', '')
   end
-  puts "Added #{count}"
+
+  field :name do
+    tds[1].css('a').text.tidy
+  end
+
+  field :area do
+    tds[0].text.tidy.sub(/ C\*?$/, ' Central').split(' - ').last
+  end
+
+  field :area_id do
+    tds[0].text.split(' - ').first.tidy
+  end
+
+  field :party_id do
+    unbracket(tds[1].text.tidy).last
+  end
+
+  field :party do
+    party_id
+  end
+
+  field :executive do
+    exec = tds[2].text.tidy
+    return '' if exec == 'Backbencher'
+    exec
+  end
+
+  field :source do
+    tds[1].at_css('a/@href').text
+  end
+
+  private
+
+  def tds
+    noko.css('td')
+  end
+
+  def unbracket(str)
+    cap = str.match(/^(.*?)\s*\((.*?)\)\s*$/) or return [str, 'Independent']
+    cap.captures
+  end
 end
+
+class MemberPage < Scraped::HTML
+  decorator Scraped::Response::Decorator::AbsoluteUrls
+
+  field :email do
+    noko.at_css('div.content a[href*=mailto]/@href').to_s.gsub(/[[:space:]]/, ' ').tidy.sub('mailto:', '')
+  end
+
+  field :image do
+    noko.css('li.PBItem div.content h1').xpath('./following::img[1]/@src').text
+  end
+end
+
+def scrape(h)
+  url, klass = h.to_a.first
+  klass.new(response: Scraped::Request.new(url: url).response)
+end
+
+start = 'http://www.parliament.bm/Members_of_Parliament.aspx'
+data = scrape(start => MembersPage).members.map do |mem|
+  mem.to_h.merge(scrape(mem.source => MemberPage).to_h).merge(term: 2012)
+end
+# puts data.map { |r| r.sort_by { |k, _| k }.to_h }
 
 ScraperWiki.sqliteexecute('DELETE FROM data') rescue nil
-scrape_list('http://www.parliament.bm/Members_of_Parliament.aspx')
+ScraperWiki.save_sqlite(%i(id term), data)
+puts "Added #{data.count}"
